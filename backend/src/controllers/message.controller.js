8d1bc914bbf0be87d453240cb4a9c64e029ab1fb
dead-shadow-7 <deadshadow12119@gmail.com>
2025-07-11 22:41:41 +0530

    Added getUserForSidebar logic

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,18 @@
+import User from "../models/user.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedUserId },
+    }).select("-password");
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("Error in getUsersForSidebar controller", error.message);
+    res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+};
+
+export const getMessages = async (req, res) => {};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+
+import messageRoutes from "./routes/message.route.js";
+import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -10,6 +12,7 @@ const PORT = process.env.PORT || 5001;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
