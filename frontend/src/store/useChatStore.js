b341511755fc0575537bf137d7e0f7e17929ae61
dead-shadow-7 <deadshadow12119@gmail.com>
2025-08-27 23:05:44 +0530

    Just added lot more stuff. I am just confused now

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,8 +2,8 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 
-export const useChatStore = create((set) => ({
-  message: [],
+export const useChatStore = create((set, get) => ({
+  messages: [], // Changed from message to messages
   users: [],
   selectedUser: null,
   isUserLoading: false,
@@ -29,6 +29,18 @@ export const useChatStore = create((set) => ({
       toast.error(error.response.data.message);
     } finally {
       set({ isMessagesLoading: false });
+    }
+  },
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get(); // Changed from message to messages
+    try {
+      const res = await axiosInstance.post(
+        `/messages/send/${selectedUser._id}`,
+        messageData
+      );
+      set({ messages: [...messages, res.data] }); // Changed from message to messages
+    } catch (error) {
+      toast.error(error.response.data.message);
     }
   },
   setSelectedUser: (selectedUser) => set({ selectedUser }),
