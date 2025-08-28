588cd848765286fde945415ec95aaa3ac2b31886
dead-shadow-7 <deadshadow12119@gmail.com>
2025-08-28 16:30:00 +0530

    Updated axios.js so the backend url is saved in env file

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api", // Adjust the base URL as needed
+  baseURL: import.meta.env.VITE_BACKEND_URL, // Adjust the base URL as needed
   withCredentials: true,
 });
